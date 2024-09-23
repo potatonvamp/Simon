242696f5e43319f659f16b2d9136d83aa1834da7
potatonvamp <2026cambo.oly@fcboe.org>
2024-09-23 13:45:36 -0400

    Update Simon.py
    Changed my functions to prepare for new code

--- Simon.py
+++ Simon.py
@@ -19,46 +19,21 @@ red = DigitalInOut(board.D5)
 red.direction = Direction.OUTPUT
 red.value = False
 
-White = DigitalInOut(board.D6)
+White = DigitalInOut(board.D10)
 White.direction = Direction.INPUT
 
-Black = DigitalInOut(board.D7)
+Black = DigitalInOut(board.D11)
 Black.direction = Direction.INPUT
 
-Orange = DigitalInOut(board.D8)
-Orange.direction = Direction.INPUT
 
-my_list = []
 
-def blink(me):
-    me.value = not me.value
-    time.sleep(.3)
-    me.value = not me.value
-    time.sleep(.3)
-   
-def add_to_sequence(yes):
-   yes.append(random.randint(0, 3))
+sequence = [0,3]
 
-def display_sequence(yes):
-   for item in yes:
-       if item == 0:
-           blink(blue)
-       if item == 1:
-           blink(yellow)
-       if item == 2:
-           blink(green)
-       if item == 3:
-           blink(red)
+def add():
+    pass
+    
+def game_over():
+    
 
-while True:
-   if  Black.value:
-       time.sleep(.3)
-       add_to_sequence(my_list)
-       print(my_list)
-   elif White.value:
-       time.sleep(.3)
-       display_sequence(my_list)
-       print(my_list)
-   elif Orange.value:
-       time.sleep(.3)
-       my_list = []
+def display_sequence():
+    
