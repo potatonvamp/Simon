ead45b72317165530b536dfea34d5bce076826c1
potatonvamp <2026cambo.oly@fcboe.org>
2024-09-25 13:46:41 -0400

    Update Simon.py
    made lights actually blink randomly

--- Simon.py
+++ Simon.py
@@ -19,29 +19,76 @@ red = DigitalInOut(board.D5)
 red.direction = Direction.OUTPUT
 red.value = False
 
-White = DigitalInOut(board.D10)
-White.direction = Direction.INPUT
+bluebtn = DigitalInOut(board.D9)
+bluebtn.direction = Direction.OUTPUT
+bluebtn.value = False
 
-Black = DigitalInOut(board.D11)
-Black.direction = Direction.INPUT
+yellowbtn = DigitalInOut(board.D8)
+yellowbtn.direction = Direction.OUTPUT
+yellowbtn.value = False
+
+greenbtn = DigitalInOut(board.D7)
+greenbtn.direction = Direction.OUTPUT
+greenbtn.value = False
+
+redbtn = DigitalInOut(board.D6)
+redbtn.direction = Direction.OUTPUT
+redbtn.value = False
+
+white = DigitalInOut(board.D10)
+white.direction = Direction.INPUT
 
 
+points = 0
 sequence = []
+
 
 def blink(light):
     light.value = not light.value
     time.sleep(.6)
-    light.value = not light.vlue
+    light.value = not light.value
     time.sleep(.6)
     
 
-def add_sequence():
-    sequence.append(random.randint(0,3))
+def add_sequence(me):
+    me.append(random.randint(0,3))
     
+def user():
+    while not bluebtn.value and not yellowbtn.value and not greenbtn.value and not redbtn.value:
+        pass
+    if bluebtn.value:
+        blink(blue)
+        return 0
+    if yellowbtn.value:
+        blink(yellow)
+        return 1
+    if greenbtn.value:
+        blink(green)
+        return 2
+    if redbtn.value:
+        blink(red)
+        return 3
+        
 def display_sequence(me):
-    add_sequence
-    
-def game_start():
-    while True:
-        if White.value:
+    for x in me:
+        if x == 0:
+            blink(blue)
+        if x == 1:
+            blink(yellow)
+        if x == 2:
+            blink(green)
+        if x == 3:
+            blink(blue)
             
+def game_start():
+    white.value
+    
+while True:
+    if not game_start and :
+        add_sequence(me)
+        display_sequence(sequence)
+        input = user()
+            
+
+
+game_start()
